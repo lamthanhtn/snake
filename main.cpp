4370be4d5c7e60ffb47efcb94841dd0dbb1361b2
Tony T. <lamthanhtn@gmail.com>
2026-04-20 10:02:46 +0700

    Refactor main.cpp for Unix compatibility

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <conio.h>
-#include <windows.h>
-#include <time.h>
+#include <vector>
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <ctime>
+#include <cstring> // Thư viện cần thiết để dùng memset sửa lỗi -Wnonnull
 
 using namespace std;
 
-// Định nghĩa các hằng số khung hình [cite: 131, 133, 135, 137]
+// Định nghĩa các hằng số khung hình
 #define MINX 2
 #define MINY 2
 #define MAXX 35
@@ -15,63 +18,104 @@ struct Point {
     int x, y;
 };
 
+// Hàm mô phỏng gotoxy cho môi trường terminal Unix
+void gotoxy(int x, int y) {
+    printf("\033[%d;%dH", y, x);
+}
+
+// Hàm mô phỏng kbhit() cho Linux/Codespaces
+int kbhit(void) {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+    
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    
+    ch = getchar();
+    
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+    
+    if(ch != EOF) {
+        ungetc(ch, STDIN_FILENO);
+        return 1;
+    }
+    return 0;
+}
+
+// Hàm mô phỏng getch() - Đã sửa lỗi argument 2 null
+char getch(void) {
+    char buf = 0;
+    struct termios old;
+    // Sửa lỗi: Khởi tạo bộ nhớ sạch cho struct thay vì gán {0}
+    memset(&old, 0, sizeof(struct termios)); 
+    
+    if (tcgetattr(0, &old) < 0) return 0;
+    
+    struct termios new_settings = old;
+    new_settings.c_lflag &= ~ICANON;
+    new_settings.c_lflag &= ~ECHO;
+    
+    tcsetattr(0, TCSANOW, &new_settings);
+    if (read(0, &buf, 1) < 0) buf = 0;
+    tcsetattr(0, TCSANOW, &old);
+    
+    return buf;
+}
+
 class Snake {
 public:
     Point A[100];
     int DoDai;
-
+    
     Snake() {
         DoDai = 3;
-        A[0] = {10, 10};
-        A[1] = {9, 10};
+        A[0] = {10, 10}; 
+        A[1] = {9, 10}; 
         A[2] = {8, 10};
     }
-
-    // Hàm di chuyển và xử lý ăn mồi [cite: 209, 212, 213, 214, 215, 216]
+    
     void DiChuyen(int Huong, Point& Qua) {
-        for (int i = DoDai - 1; i > 0; i--)
+        for (int i = DoDai - 1; i > 0; i--) {
             A[i] = A[i - 1];
-
-        if (Huong == 0) A[0].x = A[0].x + 1;      // Phải
-        if (Huong == 1) A[0].y = A[0].y + 1;      // Xuống
-        if (Huong == 2) A[0].x = A[0].x - 1;      // Trái
-        if (Huong == 3) A[0].y = A[0].y - 1;      // Lên
-
-        // Kiểm tra ăn mồi 
-        if ((A[0].x == Qua.x) && (A[0].y == Qua.y)) {
+        }
+        
+        if (Huong == 0) A[0].x++; // Phải
+        if (Huong == 1) A[0].y++; // Xuống
+        if (Huong == 2) A[0].x--; // Trái
+        if (Huong == 3) A[0].y--; // Lên
+        
+        // Kiểm tra ăn mồi
+        if (A[0].x == Qua.x && A[0].y == Qua.y) {
             DoDai++;
-            Qua.x = rand() % (MAXX - MINX) + MINX;
-            Qua.y = rand() % (MAXY - MINY) + MINY;
+            Qua.x = rand() % (MAXX - MINX + 1) + MINX;
+            Qua.y = rand() % (MAXY - MINY + 1) + MINY;
         }
     }
-
-    // Hàm vẽ con rắn và mồi [cite: 170, 175, 178, 183]
+    
     void Ve(Point Qua) {
         for (int i = 0; i < DoDai; i++) {
             gotoxy(A[i].x, A[i].y);
-            if (i == 0) cout << "O"; // Đầu rắn
-            else cout << "x";        // Thân rắn [cite: 178]
+            if (i == 0) cout << "O";
+            else cout << "x";
         }
         gotoxy(Qua.x, Qua.y);
-        cout << "*"; // Quả mồi [cite: 183]
-    }
-
-    void gotoxy(int x, int y) {
-        COORD coord;
-        coord.X = x;
-        coord.Y = y;
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+        cout << "*";
     }
 };
 
-// Hàm vẽ khung trò chơi [cite: 139, 150, 151, 152, 154]
 void VeKhung() {
     for (int i = MINX - 1; i <= MAXX + 1; i++) {
         for (int j = MINY - 1; j <= MAXY + 1; j++) {
             if (i == MINX - 1 || i == MAXX + 1 || j == MINY - 1 || j == MAXY + 1) {
-                COORD coord = {(short)i, (short)j};
-                SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-                printf("+"); [cite: 154]
+                gotoxy(i, j);
+                printf("+");
             }
         }
     }
@@ -82,35 +126,41 @@ int main() {
     Point Qua;
     int Huong = 0; // 0: Phải, 1: Xuống, 2: Trái, 3: Lên
     char t;
-
-    srand((int)time(0)); [cite: 220]
-    Qua.x = rand() % (MAXX - MINX) + MINX; [cite: 220]
-    Qua.y = rand() % (MAXY - MINY) + MINY; [cite: 220]
-
-    while (1) { [cite: 220]
-        if (kbhit()) { [cite: 220]
-            t = getch(); [cite: 220]
+    
+    srand(time(0));
+    Qua.x = rand() % (MAXX - MINX + 1) + MINX;
+    Qua.y = rand() % (MAXY - MINY + 1) + MINY;
+    
+    // Xóa màn hình ban đầu
+    printf("\033[2J"); 
+    
+    while (1) {
+        if (kbhit()) {
+            t = getch();
             if (t == 'a' && Huong != 0) Huong = 2;
-            if (t == 'd' && Huong != 2) Huong = 0;
-            if (t == 'w' && Huong != 1) Huong = 3;
-            if (t == 's' && Huong != 3) Huong = 1;
-            if (t == 'x') break; // Thoát [cite: 220]
+            else if (t == 'd' && Huong != 2) Huong = 0;
+            else if (t == 'w' && Huong != 1) Huong = 3;
+            else if (t == 's' && Huong != 3) Huong = 1;
+            else if (t == 'x') break;
         }
-
-        system("cls"); [cite: 156]
-        VeKhung(); [cite: 158]
-        r.Ve(Qua); [cite: 220]
-        r.DiChuyen(Huong, Qua); [cite: 220]
-
+        
+        printf("\033[2J"); // Xóa màn hình chuẩn ANSI
+        VeKhung();
+        r.Ve(Qua);
+        r.DiChuyen(Huong, Qua);
+        
         // Kiểm tra va chạm khung
         if (r.A[0].x < MINX || r.A[0].x > MAXX || r.A[0].y < MINY || r.A[0].y > MAXY) {
-            system("cls");
+            gotoxy(1, MAXY + 2);
             cout << "GAME OVER!" << endl;
             break;
         }
-
-        Sleep(200); [cite: 164]
+        
+        // Đẩy dữ liệu ra terminal ngay lập tức
+        fflush(stdout);
+        // Tốc độ chơi (150ms)
+        usleep(150000); 
     }
-
+    
     return 0;
 }
